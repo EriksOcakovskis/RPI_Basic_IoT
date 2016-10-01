bb15ba9790b8bcc7ab2a3b5e06bb4727dcce38e0
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-10-02 00:13:02 +0300

    Add color picker via web, improve code
    Added simple template system
    Improved comments
    Simple color picker via web interface

--- fserver.py
+++ fserver.py
@@ -1,30 +1,83 @@
 import RPi.GPIO as GPIO
 
+# Manipulate 4 pin shared anode LED with Raspberry Pi
+
+# Lauri rig pins
 ledB = 27
 ledG = 22
 ledR = 10
+# Test rig pins
+# ledB = 6
+# ledG = 5
+# ledR = 4
 
+# Enabling pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledG, GPIO.OUT)
 GPIO.setup(ledR, GPIO.OUT)
 GPIO.setup(ledB, GPIO.OUT)
 
+# Sends current to all pins disabling all lights
 def cleanLed():
     GPIO.output(ledG, True)
     GPIO.output(ledR, True)
     GPIO.output(ledB, True)
 
+cleanLed()
+
+# Setting up all leds with 1000Hz
+ledGreenDim = GPIO.PWM(ledG, 1000)
+ledBlueDim = GPIO.PWM(ledB,1000)
+ledRedDim = GPIO.PWM(ledR,1000)
+
+# Setting up all leds, duty cycle to max
+ledGreenDim.start(100.0)
+ledBlueDim.start(100.0)
+ledRedDim.start(100.0)
+
 def redLed():
-    GPIO.output(ledR, False)
+    cleanLed()
+    ledRedDim.ChangeDutyCycle(0.0)
 
 def blueLed():
-    GPIO.output(ledB, False)
+    cleanLed()
+    ledBlueDim.ChangeDutyCycle(0.0)
 
 def greenLed():
-    GPIO.output(ledG, False)
+    cleanLed()
+    ledGreenDim.ChangeDutyCycle(0.0)
+
+# Will set LED values in duty cycle according to provided RGB value in decimal
+def pickLed(r,g,b):
+    cleanLed()
+    ledRedDim.ChangeDutyCycle(100.0)
+    ledBlueDim.ChangeDutyCycle(100.0)
+    ledGreenDim.ChangeDutyCycle(100.0)
+    r_led = 100 - ((100.0/255.0)*r)
+    g_led = 100 - ((100.0/255.0)*g)
+    b_led = 100 - ((100.0/255.0)*b)
+    ledRedDim.ChangeDutyCycle(r_led)
+    ledGreenDim.ChangeDutyCycle(g_led)
+    ledBlueDim.ChangeDutyCycle(b_led)
+    print r_led
+    print g_led
+    print b_led
 
 # make_server is used to create this simple python webserver
 from wsgiref.simple_server import make_server
+from cgi import parse_qs
+import os
+
+# Check current path
+current_dir = os.path.dirname(os.path.realpath(__file__))
+# Template file
+index_html = os.path.join(current_dir, 'index.html')
+
+# Server template as a string
+def serveHtml(file):
+    with open(file, 'r') as myfile:
+      data=myfile.read()
+    return data
 
 # Function that is ran when a http request comes in
 def simple_app(env, start_response):
@@ -39,67 +92,46 @@ def simple_app(env, start_response):
         print("user asked for /on")
         cleanLed()
         redLed()
-        return '<!DOCTYPE html>'\
-                '<html>'\
-                    '<head>'\
-                        '<title>"Lights"</title>'\
-                    '</head>'\
-                    '<body>'\
-                        '<a title="Red" href="/r">Red</a>'\
-                        '<a title="Green" href="/g">Green</a>'\
-                        '<a title="Blue" href="/b">Blue</a>'\
-                    '</body>'\
-                '</html>'
+        return serveHtml(index_html)
 
     elif env["PATH_INFO"] == "/g":
         print("user asked for /off")
         cleanLed()
         greenLed()
-        return '<!DOCTYPE html>'\
-                '<html>'\
-                    '<head>'\
-                        '<title>"Lights"</title>'\
-                    '</head>'\
-                    '<body>'\
-                        '<a title="Red" href="/r">Red</a>'\
-                        '<a title="Green" href="/g">Green</a>'\
-                        '<a title="Blue" href="/b">Blue</a>'\
-                    '</body>'\
-                '</html>'
+        return serveHtml(index_html)
 
     elif env["PATH_INFO"] == "/b":
         print("user asked for /off")
         cleanLed()
         blueLed()
-        return '<!DOCTYPE html>'\
-                '<html>'\
-                    '<head>'\
-                        '<title>"Lights"</title>'\
-                    '</head>'\
-                    '<body>'\
-                        '<a title="Red" href="/r">Red</a>'\
-                        '<a title="Green" href="/g">Green</a>'\
-                        '<a title="Blue" href="/b">Blue</a>'\
-                    '</body>'\
-                '</html>'
+        return serveHtml(index_html)
 
+    # Process a POST request with hex RGB value            
+    elif env["PATH_INFO"] == "/working":
+        print("user asked for /working")
+        request_body_size = int(env.get('CONTENT_LENGTH', 0))
+        request_body = env['wsgi.input'].read(request_body_size)
+        # Request to dictionary
+        d = parse_qs(request_body)
+        # Parse only values we expected
+        hex_color = d['led_color']
+        hex_color_sting = ''.join(hex_color)
+        # Split hex string
+        red_int = int(hex_color_sting[1:3], 16)
+        green_int = int(hex_color_sting[3:5], 16)
+        blue_int = int(hex_color_sting[5:7], 16)
+        print "red - %s green - %s blue - %s" % (red_int, green_int, blue_int)
+        pickLed(red_int, green_int, blue_int)
+        # for testing
+        # return hex_color_sting
+        return serveHtml(index_html)
     else:
         print("user asked for something else")
-        return '<!DOCTYPE html>'\
-                '<html>'\
-                    '<head>'\
-                        '<title>"Lights"</title>'\
-                    '</head>'\
-                    '<body>'\
-                        '<a title="Red" href="/r">Red</a>'\
-                        '<a title="Green" href="/g">Green</a>'\
-                        '<a title="Blue" href="/b">Blue</a>'\
-                    '</body>'\
-                '</html>'
+        return serveHtml(index_html)
 
 # Create a small python server
-httpd = make_server("", 8011, simple_app)
-print "Serving on port 8011..."
-print "You can open this in the browser http://192.168.1.xxx:8011 where xxx is your rpi ip aadress"
-print "Or if you run this server on your own computer then http://localhost:8011"
+httpd = make_server("", 8411, simple_app)
+print "Serving on port 8411..."
+print "You can open this in the browser http://192.168.1.xxx:8411 where xxx is your rpi ip aadress"
+print "Or if you run this server on your own computer then http://localhost:8411"
 httpd.serve_forever()
